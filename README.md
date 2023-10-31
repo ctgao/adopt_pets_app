af8c22e1c86a7fbcca2c646e09b9f6e1e9bb294b
cgao_ksm <137011200+ctgao@users.noreply.github.com>
2023-10-31 10:07:22 -0400

    Updated the ReadMe

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# adopt_pets_app
+# Adopt Pets App (Beginner Version)
 
 Introduction to React by following [this tutorial](https://react-v8.holt.courses/).
-This is only the BEGINNER version of this tutorial. Notes: There are a TON of versions and files because I wanted to keep track of my progress alongs the way.
+
+This is only the BEGINNER version of this tutorial. Notes: There are a TON of versions and files because I wanted to keep track of my progress along the way.
 There are tons of comments everywhere so I can try and explain what all the code does. 
--- src/App1.js
+++ src/App1.js
@@ -2,9 +2,13 @@
 // Children can't mess up the parents rendering
 // Parents can pass BAD data to children
 const Pet = (props) => {
+  // eslint-disable-next-line no-undef
   return React.createElement("div", {}, [
+    // eslint-disable-next-line no-undef
     React.createElement("h2", {}, props.name),
+    // eslint-disable-next-line no-undef
     React.createElement("h3", {}, props.owner),
+    // eslint-disable-next-line no-undef
     React.createElement("h4", {}, props.element),
   ]);
 };
@@ -13,18 +17,24 @@ const App = () => {
   // always capitalize React components: it's required
   // html tag type (div, h1, etc.), attributes for that tag, innerHTML between the open and close tag
   // the html tag type can even be a non-existent tag and React will try to render it
+
+  // eslint-disable-next-line no-undef
   return React.createElement("div", {}, [
+    // eslint-disable-next-line no-undef
     React.createElement("h1", {}, "Adopt Me Please!"),
+    // eslint-disable-next-line no-undef
     React.createElement(Pet, {
       name: "Guoba",
       owner: "Xiangling",
       element: "fire",
     }),
+    // eslint-disable-next-line no-undef
     React.createElement(Pet, {
       name: "Taroumaru",
       owner: "Kamisato clan",
       element: "physical",
     }),
+    // eslint-disable-next-line no-undef
     React.createElement(Pet, {
       name: "Yuegui",
       owner: "Yaoyao",
@@ -34,5 +44,7 @@ const App = () => {
 };
 
 const container = document.getElementById("root");
+// eslint-disable-next-line no-undef
 const root = ReactDOM.createRoot(container);
+// eslint-disable-next-line no-undef
 root.render(React.createElement(App));
